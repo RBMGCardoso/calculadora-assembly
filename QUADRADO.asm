f4132d041672e8c0711cb945bc60b61223533e88
RBMGCardoso <rubenbmgcardoso@gmail.com>
2022-12-22 18:05:12 +0000

    Update QUADRADO.asm

--- QUADRADO.asm
+++ QUADRADO.asm
@@ -1,16 +1,269 @@
-.DATA  
+.DATA
+; Welcome screen variables
+input DW 0
+welcomeString DB '               ALGORITMOS$' 
+startString DB '           PRESSIONE UM BOTAO$'
+buttonString DB '     DIVISAO             RAIZ QUADRADA$'
+paramX DW ?
+paramY DW ?
+comprimentoLim DW ?
+larguraLim DW ?
+
+; Input screen  
 width DW 35
 height DW 35
 limHeight DW ? 
 limWidth DW ?
-x DW 10
-y DW 10
+x DW 20
+y DW 100
 verticalDrawn DB 0h 
 horizontalDrawn DB 0h   
 drawnSquares DB 0h
 currentAscii DB 48
+msgDivisor DB 'Divisor: $'
+msgDividendo DB 'DIvidendo: $'
+msgResultado DB 'Resultado: $'
 
-.CODE        
+.CODE
+WELCOMEWINDOW proc
+    ;alinha a welcomeString no sitio pretendido
+    MOV CX, 7
+    CALL NEWLINE
+    
+    ;mostra welcomeString
+    mov dx, offset welcomeString
+    mov ah, 09h
+    int 21h  
+    
+    MOV CX, 2
+    call NEWLINE
+    
+    ;mostra startString
+    mov dx, offset startString
+    mov ah, 09h
+    int 21h   
+ret 
+ENDP
+
+INSIDESQUARETEXT PROC
+    MOV CX, 5 
+    call NEWLINE 
+    mov dx, offset buttonString
+    mov ah, 09h
+    int 21h
+RET     
+ENDP
+
+verticalLines proc
+    writeVertical:
+    mov ah,0ch
+    mov al, 0fh ;color
+    mov cx, x ; x co-ordinate
+    mov dx, y ; y co-ordinate
+    mov bh,1    ; page no - critical while animating  
+    INC y
+    int 10h
+    MOV AX, larguraLim 
+    CMP y, AX
+    JBE writeVertical
+    
+    MOV AX, paramX
+    MOV BX, paramY 
+    
+    MOV x, AX
+    MOV y, BX
+    
+    writeVertical2:
+    mov ah,0ch;
+    mov al, 0fh ;color
+    mov cx, x ; x co-ordinate
+    mov dx, y ; y co-ordinate
+    mov bh,1    ; page no - critical while animating  
+    INC y
+    int 10h
+    MOV AX, larguraLim 
+    CMP y, AX
+    JBE writeVertical2
+    ret
+endp 
+
+horizontalLines proc 
+    writeHorizontal:
+    mov ah,0ch;
+    mov al, 0fh ;color
+    mov cx, x ; x co-ordinate
+    mov dx, y ; y co-ordinate
+    mov bh,1    ; page no - critical while animating  
+    INC x
+    int 10h
+    MOV AX, comprimentoLim 
+    CMP x, AX 
+    JBE writeHorizontal
+    
+    MOV AX, paramX
+    MOV BX, paramY
+     
+    MOV x, AX
+    MOV y, BX
+    
+    writeHorizontal2:
+    mov ah,0ch;
+    mov al, 0fh ;color
+    mov cx, x ; x co-ordinate
+    mov dx, y ; y co-ordinate
+    mov bh,1    ; page no - critical while animating  
+    INC x
+    int 10h 
+    MOV AX, comprimentoLim 
+    CMP x, AX 
+    JBE writeHorizontal2
+    ret
+endp
+
+;desenha o quadrado
+desenhaQuadrados proc
+    ;desenha botao da divisao
+    MOV paramX,120
+    MOV paramY,100
+    MOV larguraLim,135
+    MOV comprimentoLim,120
+    CALL verticalLines
+    
+    MOV x, 20
+    MOV y, 100
+    MOV paramX,20
+    MOV paramY,135
+    CALL horizontalLines
+    
+    ;desenha botao da raiz quadrada
+    mov x, 190
+    mov y, 100
+    MOV paramX,310
+    MOV paramY,100
+    MOV larguraLim,135
+    MOV comprimentoLim,310
+    CALL verticalLines
+    
+    MOV x, 190
+    MOV y, 100
+    MOV paramX,190
+    MOV paramY,135
+    CALL horizontalLines 
+      
+    ret   
+endp
+
+recebeMouseKeyboardInput proc      
+    ;se o utilizador pressionar 1, permite escolher o algoritmo pelo teclado, se pressionar 2 , permite escolher o algoritmo pelo rato     
+    choiceError:
+    mov ah,00h
+    int 16h
+    
+    CMP al,031H
+    JE keyboardInput 
+    
+    CMP al,032H
+    JE mouseLoop
+    JNE choiceError
+    
+    keyboardInput:
+    mov ah,00h
+    int 16h
+    
+    CMP al, 031H
+    JE confirmDivision
+    
+    CMP al, 032H
+    JE confirmSqrt
+    JNE keyBoardInput
+     
+    ;inicializa o rato 
+    MOV AX,00H
+    INT 33H  
+    
+    mouseLoop: 
+    ;mostra cursor
+    MOV AX, 01H
+    INT 33H   
+    
+    ;recebe a posicao do cursor
+    MOV AX, 03H
+    INT 33H
+    
+    ;verifica se foi clicado no botao esquerdo do rato
+    CMP BX, 1 
+    JNE mouseLoop
+    
+    ;verifica as coordenadas X para ver se está dentro do botão esquerdo
+    CMP CX, 028H
+    JL checkRightButton
+        
+    CMP CX, 0F0H
+    JG checkRightButton
+    
+       
+    ;verifica as coordenadas Y para ver se está dentro do botão esquerdo
+    CMP DX, 064H 
+    JL checkRightButton
+    
+    CMP DX, 086H
+    JG checkRightButton
+    
+    jmp confirmDivision
+        
+    checkRightButton:        
+    ;verifica as coordenadas X para ver se está dentro do botão
+    CMP CX, 017EH
+    JL mouseLoop
+    
+    CMP CX, 026AH
+    JG mouseLoop
+    
+    ;verifica as coordenadas Y para ver se está dentro do botão esquerdo
+    CMP DX, 064H 
+    JL mouseLoop
+    
+    CMP DX, 086H
+    JG mouseLoop
+    JMP confirmSqrt
+    
+    
+    confirmDivision:
+    ;se estiver input = 1 
+    ADD input,1 
+    JMP fimProc 
+    
+    confirmSqrt:
+    ADD input,2
+    
+    fimproc:
+ret
+endp
+
+;mostra o ecra principal do programa
+showMainScreen proc
+    CALL WELCOMEWINDOW
+    CALL desenhaQuadrados    
+    CALL insideSquareText   
+    CALL recebeMouseKeyboardInput
+    
+    CMP input, 1
+    JE jmpDivis
+    
+    CMP input, 2
+    JE jmpRaiz
+       
+    jmpDivis:
+    CALL INPUTSCREEN  
+    RET
+    
+    jmpRaiz:
+    CALL INPUTSCREEN
+    RET    
+     
+RET
+endp 
+        
 DESENHAQUADRADO PROC
     MOV AX, y              
     ADD AX, height          
@@ -78,9 +331,11 @@ DESENHAQUADRADO PROC
     SUB AX, height
     MOV y, AX  
 RET
-ENDP   
+ENDP
 
-PRINTINPUTSCREEN PROC
+   
+
+PRINTINPUTSCREEN PROC   
     drawTopSquares:         ; Desenha os 7 quadrados de cima (0-9 ; "," ; backspace)
         MOV horizontalDrawn, 0h ; Reset das flags de contagem de linhas
         MOV verticalDrawn, 0h   ; Reset das flags de contagem de linhas
@@ -127,6 +382,7 @@ RET
 ENDP
 
 ESPACOS PROC
+    MOV AH, 2
     MOV BX, 0
     espaço: 
     MOV DL, ' '
@@ -138,68 +394,136 @@ ESPACOS PROC
 ENDP
 
 NEWLINE PROC
-    MOV AH, 2
-    MOV DL, 10
-    INT 21H
-    MOV DL, 13
-    INT 21H
+    MOV BX, 0
+    newLineLoop:
+        MOV AH, 2
+        MOV DL, 10
+        INT 21H
+        MOV DL, 13
+        INT 21H
+        INC BX
+    CMP BX, CX
+    JL newLineLoop
 RET
 ENDP
 
-
-MAIN PROC  
-    MOV DX, @DATA           ; Variaveis
-    MOV DS, DX           
-    
-    mov ah, 0               ; Inicialização do modo gráfico
-    mov al, 13h
-    int 10h
-        
-    CALL NEWLINE
-    CALL NEWLINE
+DRAWNUMBERS PROC    
+    MOV CX, 3       ; CX representa o número de linhas a adicionar
     CALL NEWLINE
     
-    drawNumbers:
-    MOV CX, 4 
+    drawTopNumbers:
+    MOV CX, 4       ; CX representa o número de espaços a adicionar
     CALL ESPACOS
     MOV DL, currentAscii
     INC currentAscii
     INT 21H
     CMP currentAscii, 52
-    JLE drawNumbers
+    JLE drawTopNumbers
     
-    MOV CX, 5
+    MOV CX, 5       ; CX representa o número de espaços a adicionar
     CALL ESPACOS
     MOV DL, 45
     INT 21H
     
-    MOV CX, 5
+    MOV CX, 5       ; CX representa o número de espaços a adicionar
     CALL ESPACOS
     MOV DL, 174
     INT 21H
     
-    CALL NEWLINE
-    CALL NEWLINE
-    CALL NEWLINE
-    CALL NEWLINE
+    MOV CX, 5       ; CX representa o número de linhas a adicionar
     CALL NEWLINE
     
-    drawNumbers2:
+    drawBotNumbers:
     MOV CX, 4 
     CALL ESPACOS
     MOV DL, currentAscii
     INC currentAscii
     INT 21H
     CMP currentAscii, 57
-    JLE drawNumbers2
+    JLE drawBotNumbers
     
     MOV CX, 5
     CALL ESPACOS
     MOV DL, 61
     INT 21H
+RET
+ENDP
+
+DRAWDIVISOR PROC    
+    MOV CX, 4
+    CALL ESPACOS
+
+    MOV CX, 6
+    CALL NEWLINE
     
-    CALL PRINTINPUTSCREEN 
-                            
+    MOV AH, 9
+    MOV DX, OFFSET msgDivisor
+    INT 21H
+        
+RET
+ENDP
+
+DRAWDIVIDENDO PROC
+    MOV CX, 4
+    CALL ESPACOS
+
+    MOV CX, 2
+    CALL NEWLINE
+    
+    MOV AH, 9
+    MOV DX, OFFSET msgDividendo
+    INT 21H
+        
+RET
+ENDP
+
+DRAWRESULTADO PROC
+    MOV CX, 4
+    CALL ESPACOS
+
+    MOV CX, 3
+    CALL NEWLINE
+    
+    MOV AH, 9
+    MOV DX, OFFSET msgResultado
+    INT 21H
+        
+RET
+ENDP
+
+INPUTSCREEN PROC
+    mov ah, 0               ; Inicialização do modo gráfico
+    mov al, 13h
+    int 10h
+    
+    MOV AX, 0
+    MOV BX, 0
+    MOV CX, 0
+    MOV DX, 0
+    
+    MOV width, 35
+    MOV height, 35 
+    MOV x, 10
+    MOV y, 10
+    
+    CALL DRAWNUMBERS
+    CALL PRINTINPUTSCREEN
+    CALL DRAWDIVISOR
+    
+    CALL DRAWDIVIDENDO
+    CALL DRAWRESULTADO
+RET
+ENDP 
+
+MAIN PROC  
+    MOV DX, @DATA           ; Variaveis
+    MOV DS, DX                             
+    
+    mov ah, 0               ; Inicialização do modo gráfico
+    mov al, 13h
+    int 10h
+    
+    CALL showMainScreen                           
 ENDP
 END MAIN                                                    
 
